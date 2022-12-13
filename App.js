c2a7780cdc21aae5786a39f3670d94d5a259db5a
Md Darudul Islam <darudulislam@gmail.com>
2022-12-13 14:50:41 +0800

    flatList added

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import { useState } from 'react';
-import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 
 const itemList = [
-  { key: 1, item: 'Item 1' },
-  { key: 2, item: 'Item 2' },
-  { key: 3, item: 'Item 3' },
-  { key: 4, item: 'Item 4' },
-  { key: 5, item: 'Item 5' },
-  { key: 6, item: 'Item 6' },
-  { key: 7, item: 'Item 7' },
-  { key: 8, item: 'Item 8' },
-  { key: 9, item: 'Item 9' },
-  { key: 10, item: 'Item 10' },
-  { key: 11, item: 'Item 11' },
-  { key: 12, item: 'Item 12' },
-  { key: 13, item: 'Item 13' },
-  { key: 14, item: 'Item 14' },
-  { key: 15, item: 'Item 15' },
+  { name: 'Item 1' },
+  { name: 'Item 2' },
+  { name: 'Item 3' },
+  { name: 'Item 4' },
+  { name: 'Item 5' },
+  { name: 'Item 6' },
+  { name: 'Item 7' },
+  { name: 'Item 8' },
+  { name: 'Item 9' },
+  { name: 'Item 10' },
+  { name: 'Item 11' },
+  { name: 'Item 12' },
+  { name: 'Item 13' },
+  { name: 'Item 14' },
+  { name: 'Item 15' },
 ]
 export default function App() {
   const [items, setItems] = useState(itemList);
@@ -26,34 +26,51 @@ export default function App() {
 
   const onRefresh = () => {
     setRefreshing(true)
-    setItems([...items, { key: 16, item: 'Item 16' }])
+    setItems([...items, { name: 'Item 16' }])
     setRefreshing(false)
   }
 
   return (
-    <View style={styles.container}>
+    <FlatList
 
-      <ScrollView
-        refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-            colors={['#ff00ff']}
-          />
-        }
-      >
+      keyExtractor={(item, index) => index.toString()}
+      data={items}
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          colors={['#ff00ff']}
+        />
+      }
+    />
+    // <View style={styles.container}>
 
-        {
-          items.map(object => {
-            return (
-              <View style={styles.item} key={object.key}>
-                <Text style={styles.text}>{object.item}</Text>
-              </View>
-            )
-          })
-        }
-      </ScrollView>
-    </View>
+    //   <ScrollView
+    //     refreshControl={
+    //       <RefreshControl
+    //         refreshing={refreshing}
+    //         onRefresh={onRefresh}
+    //         colors={['#ff00ff']}
+    //       />
+    //     }
+    //   >
+
+    //     {
+    //       items.map(object => {
+    //         return (
+    //           <View style={styles.item} key={object.key}>
+    //             <Text style={styles.text}>{object.item}</Text>
+    //           </View>
+    //         )
+    //       })
+    //     }
+    //   </ScrollView>
+    // </View>
   );
 }
 
