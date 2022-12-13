e58fc7fee1b93cc6aa098f3c2d031266fbdc8766
Md Darudul Islam <darudulislam@gmail.com>
2022-12-13 12:09:01 +0800

    flex style added

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Style Text')
+
+  const onClickHandler = () => {
+    setName('Style Text is Done!')
+  }
   return (
-    <View style={styles.container}>
-      <Text>text project !</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    backgroundColor: '#0000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view1: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    backgroundColor: "#00ffff",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    backgroundColor: "#ff00ff",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    backgroundColor: "#ffff00",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "#000000",
+    fontSize: 35,
+    fontStyle: 'italic',
+    margin: 10,
+
+  },
+  button: {
+
+  }
 });
